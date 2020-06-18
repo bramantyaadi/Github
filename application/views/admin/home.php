53abb6b491514f49169c6ca1fcbc89c54d156379
bramantya <bramantyaadi44@gmail.com>
2020-06-18 14:29:11 +0700

    Login menggunakan CSS

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -3,11 +3,15 @@
 <head>
     <title>Document</title>
 </head>
+<link rel="stylesheet" href="<?= base_url('css/admin/style.css') ?>">
 <body>
-    <form action="" method="post">
-        <input type="text" name="" id="" placeholder="Nama">
-        <input type="text" name="" id="" placeholder="Password">
-        <input type="submit" value="Login">
-    </form>
+    <div class="login">
+        <h1>Login</h1>
+        <form method="post" action="<?= site_url('site/cekLogin') ?>"> 
+            <input type="text" name="u" placeholder="Username"/>
+            <input type="password" name="p" placeholder="Password" />
+            <button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
+        </form>
+    </div>
 </body>
 </html>
